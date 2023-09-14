0176b919f4b800a7308f226e43e31821c48ab95c
Stroie Vlad <stroie.vlad99@gmail.com>
2023-09-14 22:40:12 +0300

    configuring visual studio folders and adding files for production

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+settings_module = 'backend.production' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
